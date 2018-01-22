c7ca4d040524d47ba3ef119ccf7a882b74164fb0
Facundo Guerrero <guerremdq@gmail.com>
2018-01-22 10:35:14 +0100

    start working on support for jobs object

---
+++ lib/kube_objs/job.py
@@ -0,0 +1,47 @@
+# (c) Copyright 2017-2018 OLX
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from kube_obj import KubeObj, KubeSubObj, order_dict
+from kube_types import *
+from .environment import *
+from .pod import *
+from .selectors import *
+from .mixins import EnvironmentPreProcessMixin
+
+
+
+class Job(KubeObj):
+    apiVersion = 'batch/v1'
+    kind = 'Job'
+    kubectltype = 'job'
+
+    _defaults = {
+        'activeDeadlineSeconds': 100,
+        'backoffLimit': 5,
+        'parallelism': 1,
+        'completions': 1,
+        'pod_template': PodTemplateSpec(),
+        }
+
+    _types = {
+        'pod_template': PodTemplateSpec,
+        }
+
+    _parse_default_base = ('spec',)
+
+    _parse = {
+        'pod_template': ('spec', 'template'),
+        }
+
+    _exclude = {
+        '.status': True,
+        }
+
+    def render(self):
+        ret = self.renderer(mapping={'pod_template': 'template'})
+        del ret['name']
+        return {'metadata': {'name': self._data['name']}, 'spec': ret}
